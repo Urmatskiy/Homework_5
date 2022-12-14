a0b666ffb837732f211d1b34c66f55ec3e28a947
Urmatskiy <nickiton14@gmail.com>
2022-12-14 09:41:50 +0300

    Выполнил 34 задание

---
+++ task_34/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateMassive(int lenght, int first, int last)
+{
+    int[] arr = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        arr[i] = new Random().Next(first, last + 1);
+    }
+    return arr;
+}
+
+int NumberOfEvenDigits(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) counter++; 
+    }
+    return counter;
+}
+
+int[] array = CreateMassive(10, 1, 10);
+int result = NumberOfEvenDigits(array);
+
+Console.WriteLine($"[{string.Join(", ", array)}] -> {result}");
